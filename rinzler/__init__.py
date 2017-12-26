ce0755f409f7b853f57e653ca9462f98f1f3c417
Rinzler <feliphezion@gmail.com>
2017-12-26 18:53:15 -0300

    Initialization correction.
    Signed-off-by: Rinzler <feliphezion@gmail.com>

--- rinzler/__init__.py
+++ rinzler/__init__.py
@@ -4,17 +4,25 @@ from .logger.log import setup_logging
 from .core.url_assembler import UrlAssembler
 
 __name__ = "Rinzler REST Framework"
-__version__ = "1.12.0"
+__version__ = "1.20.3"
 __author__ = "Rinzler"
 
 
 def boot(app_name):
+    """
+    Start Rinzler App
+    :param app_name: str
+    :return: dict
+    """
     setup_logging()
     url_assembler = UrlAssembler().set_app_name(app_name)
 
-    logging.getLogger(app_name).info("App booted =)")
+    logger = logging.getLogger(app_name)
+
+    logger.info("App booted =)")
 
     return {
         'url_assembler': url_assembler,
-        'app_name': app_name
+        'app_name': app_name,
+        'log': logger
     }
