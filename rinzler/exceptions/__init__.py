00052bb04c1b7283cb4bbc8b0a806afc35a18403
Rinzler <feliphezion@gmail.com>
2018-07-14 16:42:28 -0300

    Add RinzlerHttpException and updated InvalidInputException to subclass it.
    Signed-off-by: Rinzler <feliphezion@gmail.com>

---
+++ rinzler/exceptions/__init__.py
@@ -0,0 +1,24 @@
+"""
+Rinzler's exceptions module
+:author: Rinzler<github.com/feliphebueno>
+:date: 14/07/2018
+"""
+__author__ = "Rinzler<github.com/feliphebueno>"
+
+
+class RinzlerHttpException(BaseException):
+    """
+    Rinzler's base HTTP Exception, all other HTTP Exceptions must subclass this class as well as override it's
+    properities status_code and exception_name in order to be properly converted to a HTTP Error Response to the client.
+    """
+    status_code = 0
+    exception_name = "Cataclysmic Error"
+
+    def __int__(self):
+        return int(self.status_code)
+
+    def __str__(self):
+        return str(self.exception_name)
+
+    def __repr__(self):
+        return self.__str__()
--- rinzler/exceptions/invalid_input_exception.py
+++ rinzler/exceptions/invalid_input_exception.py
@@ -2,11 +2,14 @@
 Exception raised when a body request can not be decode from JSON and/or it's content doesn't match the minimum
 specified values by the app
 """
+from rinzler.exceptions import RinzlerHttpException
+
 __author__ = "Rinzler<github.com/feliphebueno>"
 
 
-class InvalidInputException(BaseException):
+class InvalidInputException(RinzlerHttpException):
     """
     InvalidInputException
     """
-    pass
+    status_code = 400
+    exception_name = "Bad Request"
